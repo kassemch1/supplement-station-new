0cc26df08b5ee91c7cc7a1f69849834f5a336592
ali mawassi <202112066@ua.edu.lb>
2024-07-30 14:58:44 +0300

    convert user interface to blade

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\user_controllers\HomeController;
+use App\Http\Controllers\user_controllers\SingleProductController;
+use App\Http\Controllers\user_controllers\CartController;
+use App\Http\Controllers\user_controllers\CheckoutController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class,'index'])->name('home');
+Route::get('/singleProduct',[SingleProductController::class,'singleProduct'])->name('singleProduct');
+Route::get('/Cart',[CartController::class,'Cart'])->name('cart');
+Route::get('/Checkout',[CheckoutController::class,'Checkout'])->name('checkout');
